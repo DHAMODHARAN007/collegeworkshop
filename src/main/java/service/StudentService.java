19b216b66dec8515c5879891e047d3502f3e00ef
DHAMODHARAN007 <dhamo1892004@gmail.com>
2023-08-05 13:00:41 +0530

    Student list added by Dhamodharan

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -17,14 +17,26 @@ public class StudentService {
         Student student = new Student();
         List<Student> studentList = new ArrayList<Student>();
         student.setStudentId("001");
-        student.setName("name");
-        student.setCourse("MCA");
+        student.setName("Ameet");
+        student.setCourse("BCA");
         student.setStartDate("01/08/2023");
         student.setEndDate("08/01/2026");
+        Student student1 = new Student();
         studentList.add(student);
-
-
-
+        student1.setStudentId("002");
+        student1.setName("Abinesh");
+        student1.setCourse("BCA");
+        student1.setStartDate("01/08/2023");
+        student1.setEndDate("08/01/2026");
+        studentList.add(student1);
+        Student student2 = new Student();
+        studentList.add(student);
+        student2.setStudentId("003");
+        student2.setName("Arun");
+        student2.setCourse("BCA");
+        student2.setStartDate("01/08/2023");
+        student2.setEndDate("08/01/2026");
+        studentList.add(student2);
     }
 
     public List<Student> getStudents(){
